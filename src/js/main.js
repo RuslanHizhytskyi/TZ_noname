ccfa96a48982fa4e9a86a8ba33e525045a6af446
RuslanHizhytskyi <r.hizhytskyi@gmail.com>
2020-12-17 21:35:42 +0200

    desctop firrst block

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
-import './helpers/postDate';
-import scrollSmooth from './helpers/smooth-scroll.js';
+// import './helpers/postDate';
+// import scrollSmooth from './helpers/smooth-scroll.js';
 
-function main() {
-  scrollSmooth();
-}
+// function main() {
+//   scrollSmooth();
+// }
 
-if (document.documentElement.clientWidth < 480) {
-  window.addEventListener('scroll',
-    function () {
-      return setTimeout(main, 1000);
-    }, {
-      once: true
-    });
-} else {
-  main();
-};
+// if (document.documentElement.clientWidth < 480) {
+//   window.addEventListener('scroll',
+//     function () {
+//       return setTimeout(main, 1000);
+//     }, {
+//       once: true
+//     });
+// } else {
+//   main();
+// };
