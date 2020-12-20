1c219be964e11588e15ed4aff17785a112148c28
RuslanHizhytskyi <r.hizhytskyi@gmail.com>
2020-12-20 14:41:59 +0200

    last fixes

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -10,65 +10,89 @@ function formValidation(formSelector, errorActiveClass) {
 
   function showError(elem, errorContainer, message) {
     elem.setAttribute('data-valid', false)
+    elem.style.outline = '2px solid red';
+    elem.style.marginBottom = '17px';
     errorContainer.textContent = message;
     errorContainer.classList.add(errorActiveClass);
   }
 
-  function hideErroe(elem, errorContainer) {
+  function hideError(elem, errorContainer) {
     elem.setAttribute('data-valid', true)
+    elem.style.cssText = '';
     errorContainer.classList.remove(errorActiveClass);
   }
   
   function validation(el) {
     const errorContainer = el.parentElement.querySelector('.form__errormessage');
     if (el.name === 'name' || el.name == 'surname') {
-      if (el.value.length < 3) {
-        showError(el, errorContainer, errorMessage.name)
-      } else {
-        hideErroe(el, errorContainer);
-      }
+      // if (el.value.length < 3) {
+      //   showError(el, errorContainer, errorMessage.name)
+      // } else {
+      //   hideErroe(el, errorContainer);
+      // }
+      el.value.length < 3
+        ? showError(el, errorContainer, errorMessage.name)
+        : hideError(el, errorContainer);
     }
 
     if (el.name === 'email') {
-      if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(el.value)) {
-        hideErroe(el, errorContainer);
-      } else {
-        showError(el, errorContainer, errorMessage.email);
-      }
+      // if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(el.value)) {
+      //   hideErroe(el, errorContainer);
+      // } else {
+      //   showError(el, errorContainer, errorMessage.email);
+      // }
+      el.value.match(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        )
+          ? hideError(el, errorContainer)
+          : showError(el, errorContainer, errorMessage.email);
+      
     }
 
     if (el.name === 'country') {
-      if (el.value === 'default') {
-        showError(el, errorContainer, errorMessage.country);
-      } else {
-        hideErroe(el, errorContainer);
-      }
+      // if (el.value === 'default') {
+      //   showError(el, errorContainer, errorMessage.country);
+      // } else {
+      //   hideErroe(el, errorContainer);
+      // }
+      el.value === "default"
+        ? showError(el, errorContainer, errorMessage.country)
+        : hideError(el, errorContainer);
     }
 
     if (el.name === 'prefix') {
-      if (el.value.trim().split('').every(el => Number.isInteger(+el))) {
-        hideErroe(el, errorContainer);
-      } else {
-        showError(el, errorContainer, errorMessage.prefix);
-      }
+      // if (el.value.trim().split('').every(el => Number.isInteger(+el))) {
+      //   hideErroe(el, errorContainer);
+      // } else {
+      //   showError(el, errorContainer, errorMessage.prefix);
+      // }
+      el.value.trim().split("").every((el) => Number.isInteger(+el))
+        ? hideError(el, errorContainer)
+        : showError(el, errorContainer, errorMessage.prefix);
+  
     }
 
-    if (el.name == 'phone') {
-      if (el.value.length > 6 && el.value.trim().split('').every(el => Number.isInteger(+el))) {
-        hideErroe(el, errorContainer);
-      } else {
-        showError(el, errorContainer, errorMessage.phone);
-      }
+    if (el.name === 'phone') {
+      // if (el.value.length > 6 && el.value.trim().split('').every(el => Number.isInteger(+el))) {
+      //   hideErroe(el, errorContainer);
+      // } else {
+      //   showError(el, errorContainer, errorMessage.phone);
+      // }
+      el.value.length > 6 && el.value.trim().split("").every((el) => Number.isInteger(+el))
+        ? hideError(el, errorContainer)
+        : showError(el, errorContainer, errorMessage.phone);
     }
   }
 
   form.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    const inputs = form.querySelectorAll('input, select'),
-          validArr = [];
+    const inputs = form.querySelectorAll('input, select');
+    let validArr = [];
+
+    
     inputs.forEach((elem, i) => {
       validation(elem);
-      elem.addEventListener('blur', () => validation(elem));
+      elem.addEventListener('change', () => validation(elem));
       validArr[i] = elem.getAttribute('data-valid');
     });
     if (validArr.every(el => el === 'true')) {
